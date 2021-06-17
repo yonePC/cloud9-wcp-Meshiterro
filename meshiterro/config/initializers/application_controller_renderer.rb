eab2f74d43cb8d4131abcf75f13ced92c479a212
yonePC <ys.mailaddress0929@gmail.com>
2021-06-17 04:39:13 +0000

    meshiterro-3章

---
+++ meshiterro/config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '9a90a8232d3cc5d60b88d633edde5c4e478150dfffeb4064bb39b5d1e7a88cab5cf5cc7998fd8d26d3730291e9698ba6f88ee15d3a370edb7f723986672ea75f'
