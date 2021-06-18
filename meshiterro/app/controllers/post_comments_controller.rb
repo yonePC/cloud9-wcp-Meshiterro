c0c6b83f08659c0a6ec042b6ab6f1754b788185d
yonePC <ys.mailaddress0929@gmail.com>
2021-06-18 06:29:08 +0000

    [update]meshiterro5章-2

--- meshiterro/app/controllers/post_comments_controller.rb
+++ meshiterro/app/controllers/post_comments_controller.rb
@@ -1,5 +1,5 @@
 class PostCommentsController < ApplicationController
-  
+
   def create
     post_image = PostImage.find(params[:post_image_id])
     comment = current_user.post_comments.new(post_comment_params)
@@ -12,13 +12,13 @@ class PostCommentsController < ApplicationController
     PostComment.find_by(id: params[:id], post_image_id: params[:post_image_id]).destroy
     redirect_to post_image_path(params[:post_image_id])
   end
-  
-  
-  
+
+
+
    private
 
   def post_comment_params
     params.require(:post_comment).permit(:comment)
   end
-  
+
 end
--- meshiterro/config/routes.rb
+++ meshiterro/config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+ # get 'users/show' #この行を削除
   devise_for :users
   root to: 'homes#top'
   resources :post_images, only: [:new, :create, :index, :show, :destroy] do
    resources :post_comments, only: [:create, :destroy]
    resource :favorites, only: [:create, :destroy]
   end
+ resources :users, only: [:show, :edit, :update]
 end
